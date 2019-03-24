b072228c862efaae48ba3071e5bb51a2468bdf02
Yuxiang Zhang <zyx1993@126.com>
2019-03-24 16:03:52 +0800

    first commit

---
+++ UDP/Assets/scripts/ProceduralGrid.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter),typeof(MeshRenderer))]
+public class ProceduralGrid : MonoBehaviour {
+	Mesh mesh;
+	Vector3[] vertices;
+	int[] triangles;
+
+	//grid settings
+	public Vector3 gridoffset;
+	float[] coo;
+
+	// Use this for initialization
+	void Awake(){
+		mesh = GetComponent<MeshFilter> ().mesh;
+	}
+	void Start(){
+		coo = UDPReceive.ins.cooInt;
+		MakeProceduralGrid();
+		UpdateMesh();
+	}
+
+
+	void MakeProceduralGrid(){
+
+
+		//get number of triangles
+		int cellCount = coo.Length/9;
+
+		//set array sizes
+		vertices = new Vector3[cellCount * 3];
+		triangles = new int[cellCount * 3];
+
+
+		//set tracker integers
+		int v = 0;
+		int t = 0;
+
+		for(int i = 0; i < cellCount ; i++)
+		{
+				//populate the vertices and triangles arrays
+			vertices [v] = new Vector3 (coo[i*9], coo[i*9+2], coo[i*9+1]) + gridoffset;
+			vertices [v+1] = new Vector3 (coo[i*9+3], coo[i*9+5], coo[i*9+4]) + gridoffset;
+			vertices [v+2] = new Vector3 (coo[i*9+6], coo[i*9+8], coo[i*9+7]) + gridoffset;
+
+				triangles [t] = v+2;
+				triangles [t+1] = v+1;
+				triangles [t+2] = v;
+				v += 3;
+				t += 3;
+			}
+
+		}
+
+
+	void UpdateMesh(){
+		mesh.Clear ();
+
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.RecalculateNormals ();
+
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		coo = UDPReceive.ins.cooInt;
+		MakeProceduralGrid();
+		UpdateMesh();
+		//print(">> " + UDPReceive.ins.textJoin);
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,11 @@
+1.First! Watch this tutorial about Vuforia AR >> https://youtu.be/mjNAPCFaZ9Y
+2.Open 'UDP >> Assests >> VuforiaUDP.unity '
+2.find 'ImageTarget' under 'Hierarchy' (usually on the left part of your unity window)
+3.Change the image target to whatever marker you uploaded in your Vuforia developer portal(also refer to the tutorial in step 1)
+4.Open 'example.gh'
+5.Run the Unity scene
+
+Hope you enjoy!
+---------------------------------------------------------------------------------------------------------------------------------
+Yuxiang Zhang
+http://zhangzhangzhang.space/
